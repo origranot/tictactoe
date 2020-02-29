e0735a2778bf9a12d334a7a00936954023de7907
OriG <oriorigranot@gmail.com>
2020-02-29 16:48:49 +0200

    Set background image

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      fa
+      asf
+      asf
+      asf
+      
+    </Container>
+  );
+}
+
+export default App;
