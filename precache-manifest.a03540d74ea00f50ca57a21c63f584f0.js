553d38299bba3c1f8aaa5483ede6c675454bc28f
Ori Granot <Oriorigranot@gmail.com>
2020-03-12 13:08:35 +0200

    Updates

---
+++ precache-manifest.a03540d74ea00f50ca57a21c63f584f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ece5b447aafc436bb4dd268305d7e0",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "bef577293671d851150c",
+    "url": "/tictactoe/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "917341cb5d8b7ce01cd5",
+    "url": "/tictactoe/static/css/main.75ab9885.chunk.css"
+  },
+  {
+    "revision": "bef577293671d851150c",
+    "url": "/tictactoe/static/js/2.f11deba8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/tictactoe/static/js/2.f11deba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917341cb5d8b7ce01cd5",
+    "url": "/tictactoe/static/js/main.9ade80d6.chunk.js"
+  },
+  {
+    "revision": "ceecb7b45847aa4147c0",
+    "url": "/tictactoe/static/js/runtime-main.b6f03ea1.js"
+  }
+]);
